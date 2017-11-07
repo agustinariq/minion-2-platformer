8e56af1f7777b06d187ae01144b87c8c99b0894a
Agustina Riquelme <agustinariq@gmail.com>
2017-11-06 23:17:56 -0300

    Enemigo con movimiento

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
+
 
 	public Transform target;
 	public float speed;
+	public SpriteRenderer spriteRenderer;
 
 	private Vector3 startPosition;
 	private Vector3 endPosition;
@@ -14,16 +16,24 @@ public class MovingPlatform : MonoBehaviour {
 	void Start () {
 		target.parent = null;
 		startPosition = transform.position;
-		endPosition = target.position;		
+		endPosition = target.position;
+		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
+
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		float deltaSpeed = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards (transform.position, target.position, deltaSpeed);
-
+		spriteFlip ();
 		if (transform.position == target.position) {
 			target.position = (target.position == startPosition) ? endPosition : startPosition;
 		}
 	}
+
+	void spriteFlip(){
+		if (target.position == startPosition) { spriteRenderer.flipX = true; }
+		if (target.position == endPosition) { spriteRenderer.flipX = false; }
+	}
 }
+
